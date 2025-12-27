b58e37724c57e57677b8aa5ead6a1c0c02d20bae
Jonathan <jonathan.s9522@gmail.com>
2025-12-27 16:13:22 -0500

    server working

---
+++ app/src/main/java/com/beatmanager/beat/manager/BeatManagerApplication.java
@@ -0,0 +1,13 @@
+package com.beatmanager.beat.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatManagerApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/beatmanager/beat/manager/BeatManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beatmanager.beat.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
