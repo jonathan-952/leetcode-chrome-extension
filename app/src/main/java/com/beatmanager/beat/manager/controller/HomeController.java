6c900240ebd353bafed6e1f03b8e8abfb655b0e1
Jonathan <jonathan.s9522@gmail.com>
2026-02-11 17:32:30 -0500

    add to db worklfow working, need to add spring security

--- app/src/main/java/com/beatmanager/beat/manager/controller/HomeController.java
+++ app/src/main/java/com/beatmanager/beat/manager/controller/HomeController.java
@@ -1,18 +1,28 @@
 package com.beatmanager.beat.manager.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.beatmanager.beat.manager.repository.UserRepository;
+import com.beatmanager.beat.manager.repository.entity.User;
+
+@RestController
+@RequestMapping("/user")
 public class HomeController {
-    
-    @RequestMapping("/")
-    @ResponseBody
-    public String index() {
-        return "hello";
+
+    private final UserRepository userRepo;
+
+    public HomeController(UserRepository userRepo) {
+        this.userRepo = userRepo;
     }
 
-
+    @PostMapping("/sign-up")
+    public User createUser(@RequestBody User user) {
+        return userRepo.save(user);
+    }
 }
 
 
--- app/src/main/java/com/beatmanager/beat/manager/repository/entity/Problem.java
+++ app/src/main/java/com/beatmanager/beat/manager/repository/entity/Problem.java
@@ -13,6 +13,10 @@ public class Problem {
 // rating
 // notes
 // 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
   @Column(nullable = false, unique = true)
   private String name;
   @Column(nullable = true, unique = false)
